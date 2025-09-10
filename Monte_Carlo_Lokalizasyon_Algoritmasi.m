bb81748ce4338d79f9650ef0be0eaf6097854c6f
OzanVahit <49794377+OzanVahit@users.noreply.github.com>
2025-09-10 15:42:14 +0300

    Monte_Carlo_Lokalizasyon_Algoritmasi.m

--- Monte_Carlo_Lokalizasyon_Algoritmasi.m
+++ Monte_Carlo_Lokalizasyon_Algoritmasi.m
@@ -15,6 +15,9 @@
 % düşme olasılıkları değişmektedir. Eğer başlangıç anında düzgün bir atama
 % olmazsa simülasyonu durdurup tekrar başlatınız!
 
+% Yüksek işlemci frekanslı/çok çekirdekli bilgisayara sahipseniz N=1500 
+% parçacıkla çalışmanız tavsiye edilir.
+
 clc
 clear all
 
@@ -62,7 +65,7 @@ m = 0;
 W(1:N) = (1/N); % Başlangıç anında robotun gerçek konumu bilinmediğinden, tüm parçacıkların robotun konumunda bulunma olasılıkları eşit ve 1/N olarak kabul edilir
 W_aci(1:N) = (1/N);
 
-cx = 0.0;  % Robotun başlangıç anında x ekseni üzerindeki gerçek konumu (metre)
+cx = 0.00;  % Robotun başlangıç anında x ekseni üzerindeki gerçek konumu (metre)
 cy = 0.78; % Robotun başlangıç anında y ekseni üzerindeki gerçek konumu (metre)
 cteta = 0; % Robotun başlangıç anındaki gerçek açı değeri (radyan)
 
@@ -715,6 +718,7 @@ p_teta(1:N) = 0;
 pteta = p_teta;
 
 end
+
 
 
 
